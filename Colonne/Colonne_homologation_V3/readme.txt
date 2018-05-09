c2fe5cee0a7bcc8c42bd20f28eb1c68e8dfe24a9
Pierre Valleau <zoubworld_arduino@yahoo.com>
2018-05-09 19:34:44 +0200

    fix compil

---
+++ Colonne/Colonne_homologation_V3/readme.txt
@@ -0,0 +1,2 @@
+pb avec plusieur cube.
+demarrer tout en meme temp genere des resets.
---
+++ final_pilo/board.h
@@ -0,0 +1,43 @@
+#ifndef __BOARD_H
+#define __BOARD_H
+#include <Wire.h>
+#include "ZTCS34725.h"
+#include "arduino.h"
+//red : A5, orange :GND, green : A5, blue : A4
+// Pick analog outputs, for the UNO these three work well
+// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
+
+
+#ifdef ARDUINO_AVR_UNO
+#define redpin -1
+#define greenpin -1
+#define bluepin -1
+
+#define MySerial Serial //usb
+#define Mywire Wire // A4 SDA //A5 SCL
+
+#elif defined(BOARD_ID_Pilo)
+
+#include <SPI.h>
+#include <variant.h>
+#include <bootloaders/boot.h>
+#define Serialpc P_COM3.serial2
+#define wirecard
+
+#define redpin -1
+#define greenpin -1
+#define bluepin -1
+
+#define MySerial P_COM3.serial2 //usb
+#define Mywire P_COM0_BIS.wire // Pcomx.pin6 SDA //Pcomx.pin5 SCL
+#define MyPin P_COM3.Pin.PIN09 //usb
+
+
+#elif defined(BOARD_ID_Captor)
+
+#else 
+#error
+
+#endif
+#define MySerialDebug MySerial
+#endif
